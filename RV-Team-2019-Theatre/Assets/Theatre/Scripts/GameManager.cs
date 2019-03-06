e73a6702c261cb7e4a301af0d12b8903a701de32
Saeh <kfournier.dev@gmail.com>
2019-03-06 17:51:51 +0100

    Gestion du passage d'un acte ou d'une scene à l'autre. Préparation de la fermeture/ouverture des ridaux.

--- RV-Team-2019-Theatre/Assets/Theatre/Scripts/GameManager.cs
+++ RV-Team-2019-Theatre/Assets/Theatre/Scripts/GameManager.cs
@@ -1,8 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Theater;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance;
+
+    private Theater.Theater currentTheater;
+    private Act currentAct;
+
+    // Theater related methods
+    public void SetTheater(Theater.Theater theater) => currentTheater = theater;
+    public void NextAct() => currentTheater.NextAct();
+    public void OpenCurtain(CurtainName cn) => currentTheater.OpenCurtain(cn);
+    public void CloseCurtain(CurtainName cn) => currentTheater.CloseCurtain(cn);
+
+    // Act related methods
+    public void SetAct(Act act) => currentAct = act;
+    public void NextScene() => currentAct.NextScene();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 }
--- RV-Team-2019-Theatre/Assets/Theatre/Scripts/Theater/Curtain.cs
+++ RV-Team-2019-Theatre/Assets/Theatre/Scripts/Theater/Curtain.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 namespace Theater
 {
+
+    public enum CurtainName { Front, Back }
+
     public class Curtain : MonoBehaviour
     {
-        
+        public void Open()
+        {
+            // TODO: Implement
+        }
+
+        public void Close()
+        {
+            // TODO: Implement
+        }
     }
 }
