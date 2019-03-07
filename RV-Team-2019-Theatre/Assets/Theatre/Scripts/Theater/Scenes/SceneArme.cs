5ed39d027a01c15af8315c25e0d62c9643b903e4
Saeh <kfournier.dev@gmail.com>
2019-03-07 15:47:10 +0100

    Ajout du script pour la scène de choix d'arme. Ajout de coroutine pour gérer les dialogues et timer les actions dans les scenes.

---
+++ RV-Team-2019-Theatre/Assets/Theatre/Scripts/Theater/Scenes/SceneArme.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Theater
+{
+    public class SceneArme : Scene
+    {
+        public AudioSource NarratorFirstSpeech;
+        public AudioSource MerlinCardsSpeech;
+        public AudioSource ArthurEndSpeech;
+
+        public Agent Merlin;
+
+        public Player Player;
+
+
+        private GameManager gm;
+
+        private void Start()
+        {
+            gm = GameManager.Instance;
+            // CardManager.Instance.SetCards();
+        }
+
+        private void Update()
+        {
+
+            // Le narrateur parle, attend 2s après la fin puis ouvre les ridaux.
+            PlaySoundThen(
+                NarratorFirstSpeech,
+                () => gm.OpenCurtains(),
+                2.0f);
+
+
+            // Spawn de Merlin
+            Merlin.Spawn();
+
+            // Merlin parle
+            PlaySoundThen(MerlinCardsSpeech, null);
+
+            // Attend 2s qu'il ait commencé puis fait spawn les cartes.
+            WaitAndInvoke(2.0f, () => CardManager.Instance.SpawnCards());
+            // Ne rien faire tant qu'une carte n'a pas été séléctionée.
+
+
+            // Spawn du rocher
+            // Rien tant que l'arme n'est pas retirée.
+            // Récupération de l'arme.
+
+            PlaySoundThen(
+                ArthurEndSpeech,
+                () => OnEnd(),
+                2.0f);
+
+
+        }
+
+        public override void OnEnd()
+        {
+
+        }
+        public override void OnStart()
+        {
+
+        }
+    }
+}
