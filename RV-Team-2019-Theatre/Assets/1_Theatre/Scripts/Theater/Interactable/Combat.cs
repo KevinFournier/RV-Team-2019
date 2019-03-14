5818ba80254a3922d32399ef74456cc3dc4be1d6
DavidHamon <48216646+DavidHamon@users.noreply.github.com>
2019-03-14 16:07:30 +0100

    combat Interaction

---
+++ RV-Team-2019-Theatre/Assets/1_Theatre/Scripts/Theater/Interactable/Combat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Theater {
+    public class Combat : MonoBehaviour
+    {
+        //public Player Arthur;
+        public List<Soldats> soldats;
+        public static Combat Instance;
+        public static int currentSoldat=0;
+
+        public static bool combatFini = false;
+
+        void Awake()
+        {
+            Instance = this;
+        }
+        void Update()
+        {
+
+            if (!combatFini)
+            {
+                if (soldats[currentSoldat].dead == true)
+                {
+                    currentSoldat++;
+                }
+
+
+                if (currentSoldat >= soldats.Capacity)
+                {
+                    combatFini = true;
+                    Debug.Log("combatFini");
+                }
+            }
+        }
+       
+
+
+
+    }
+}
---
+++ RV-Team-2019-Theatre/Assets/1_Theatre/Scripts/Theater/Interactable/Soldats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Theater {
+    public class Soldats : MonoBehaviour
+    {
+        private Vector2 velocity = Vector2.zero;
+        public ParticleSystem explosionCartons;
+        public bool dead = false;
+        
+     
+        // Start is called before the first frame update
+
+
+        // Update is called once per frame
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "SwordBlade")
+            {
+
+                StartCoroutine(AnimDestruction());
+
+            }
+        }
+
+        IEnumerator AnimDestruction()
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+
+            //Play particle
+            explosionCartons.Play();
+            yield return new WaitForSeconds(4.0f);
+            gameObject.SetActive(false);
+            dead = true;
+            //Increment soldats to go 
+        }
+    }
+}
