2e2afd5ddd1a49b3fb362f06bc303bcbf888d26d
Saeh <kfournier.dev@gmail.com>
2019-03-14 17:21:18 +0100

    Finalisation des mouvements de Jésus dans la scène 2

--- RV-Team-2019-Theatre/Assets/1_Theatre/Scripts/Theater/Scenes/SceneChoixCompagnon.cs
+++ RV-Team-2019-Theatre/Assets/1_Theatre/Scripts/Theater/Scenes/SceneChoixCompagnon.cs
@@ -78,6 +78,35 @@ namespace Theater
             Companions[index].gameObject.SetActive(true);
             Arthur.Companion = Companions[index];
             Arthur.Companion.AudioSource.clip = compagnionSpeech[index];
+
+            switch (chosenCompanion)
+            { // TODO: faire ça.
+                case CompanionType.None:
+                    break;
+                case CompanionType.Merlin:
+                    break;
+                case CompanionType.Guenievre:
+                    // Wait for Guenievre Animation
+                    break;
+                case CompanionType.R2D2:
+                    // Wait for R2D2 animation
+                    break;
+                case CompanionType.Jesus:
+                    cardSelectionValidation();
+                    break;
+                case CompanionType.Brush:
+                    cardSelectionValidation();
+                    break;
+                default:
+                    cardSelectionValidation();
+                    break;
+            }
+
+            void cardSelectionValidation()
+            {
+                isCardSelected = true;
+                CardTrigger = true;
+            }
         }
 
         /// <summary>
@@ -165,10 +194,6 @@ namespace Theater
 
             Arthur.AudioSource.clip = ArthurEndSpeech;
 
-            // Seulement pour du test sans intéraction VR
-            SetCompanion(chosenCompanion);
-
-
             WaitThen(companionSpeechDelay, companionReplica);
 
             void companionReplica()
@@ -181,8 +206,10 @@ namespace Theater
             }
             void arthurReplica()
             {
-                PlaySoundThen(Arthur.AudioSource);
-                GameManager.Instance.NextScene();
+                PlaySoundThen(
+                    Arthur.AudioSource,
+                    GameManager.Instance.NextScene,
+                    Arthur.AudioSource.clip.length + 2.0f);
             }
         }
 
